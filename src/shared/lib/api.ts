b195e3f83078ba996bf71fff160e7d4a27a5ea73
ArupBharali <39222353+ArupBharali@users.noreply.github.com>
2025-06-23 02:00:30 +0530

    Add files via upload

--- src/shared/lib/api.ts
+++ src/shared/lib/api.ts
@@ -1,10 +1,13 @@
-const BASE_URL = ''//'https://api.my-remote-server.com';
+// const BASE_URL = ''//'https://api.my-remote-server.com';
+const BASE_URL = 'http://localhost:3000';
 
 export async function apiFetch<T>(endpoint: string, options?: RequestInit): Promise<T> {
+  console.log('apiFetch endpoint',`${BASE_URL}${endpoint}`);
   const res = await fetch(`${BASE_URL}${endpoint}`, {
     headers: { 'Content-Type': 'application/json' },
     ...options
   });
+  console.log('apiFetch res',{res});
 
   if (!res.ok) {
     const message = await res.text();
